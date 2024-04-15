64e2b0b008f837b41213256e833e42624fe909ae
PSouza <peterleite.dev@gmail.com>
2024-04-15 18:30:26 -0300

    Refactor UserList.test.js to use a helper function for rendering and add tests for rendering name and email of each user

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,25 +1,29 @@
 import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
-test("render one row for each user", () => {
+function renderComponent() {
   const users = [
     { name: "John Doe", email: "john@hotmail.com" },
     { name: "Jane Doe", email: "jane@hotmail.com" },
   ];
 
   render(<UserList users={users} />);
+
+  return {
+    users,
+  };
+}
+
+test("render one row for each user", () => {
+  const { users } = renderComponent();
+
   const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
   expect(rows).toHaveLength(users.length);
 });
 
 test("render the name and email of each user", () => {
-  const users = [
-    { name: "John Doe", email: "john@hotmail.com" },
-    { name: "Jane Doe", email: "jane@hotmail.com" },
-  ];
-
-  render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   users.forEach((user) => {
     expect(screen.getByText(user.name)).toBeInTheDocument();
